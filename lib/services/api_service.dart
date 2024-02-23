6d8fb846952975599e5549a2233fdb72ba1fda45
Jason <lejb1@uci.edu>
2024-02-23 14:32:29 -0800

    fixed keys

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,12 +5,13 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class APIService {
-  static const apiKey = 'ac3204e409msh17b656760a9b400p15150cjsna2ac7afe5e91';
-  static const baseURL = 'exercises-by-api-ninjas.p.rapidapi.com';
+  static const apiKey = 'bUMV2BLqvejOGcBb4FFHzQ==dPiWwbllHtqy0U1J';
+  static const baseURL = 'https://api.api-ninjas.com/v1/exercises';
+
 
   static Future<List<Exercise>> fetchExercises({String? name, String? type, String? muscle, String? difficulty, int? offset}) async {
     Map<String, String> queryParams = {};
-    //need to check if query is valid
+    // TODO? to check if query is valid
     //could check if the words are the words allowed by api ninjas
     if (name != null) queryParams['name'] = name;
     if (type != null) queryParams['type'] = type;
@@ -23,8 +24,10 @@ class APIService {
     final response = await http.get(
       uri,
       headers: {'X-Api-Key': apiKey},
+
     );
 
+    //if code is GOOD make exercise class to store the information
     if (response.statusCode == 200) {
       List<dynamic> jsonResponse = json.decode(response.body);
       return jsonResponse.map((data) => Exercise.fromJson(data)).toList();
